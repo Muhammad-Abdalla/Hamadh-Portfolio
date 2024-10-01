0172550f4c2dbfcd1c150fd53b575f36ef3cab8f
Muhammad Abdalla <168632278+Muhammad-Abdalla@users.noreply.github.com>
2024-10-01 12:29:34 +0300

    index.js

--- index.js
+++ index.js
@@ -10,44 +10,43 @@ menuIcon.onclick = () => {
 };
 
 //BELOW IS THE WEB3 SMTP CODE FOR SENDING A GMAIL MESSAGE
+const form = document.getElementById('form');
+const result = document.getElementById('result');
 
-const form = document.getElementById("form");
-const result = document.getElementById("result");
-
-form.addEventListener("submit", function (e) {
-  const formData = new FormData(form);
+form.addEventListener('submit', function(e) {
   e.preventDefault();
-
+  const formData = new FormData(form);
   const object = Object.fromEntries(formData);
   const json = JSON.stringify(object);
+  result.innerHTML = "Please wait..."
 
-  result.innerHTML = "Please Wait...";
-
-  fetch('https://api.web3forms.com/submit', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-       'Accept': 'application/json'
-    },
-    body: json
-  })
-    .then(async (response) => {
-      let json = await response.json();
-      if (response.status == 200) {
-        result.innerHTML = json.message;
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-      result.innerHTML = "Something Went Wrong!";
-    })
-    .then(function () {
-      form.reset();
-      setTimeout(() => {
-        result.style.display = "none";
-      }, 3000);
-    });
-    console.log(json);
+    fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: json
+        })
+        .then(async (response) => {
+            let json = await response.json();
+            if (response.status == 200) {
+                result.innerHTML = "Form submitted successfully";
+            } else {
+                console.log(response);
+                result.innerHTML = json.message;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            result.innerHTML = "Something went wrong!";
+        })
+        .then(function() {
+            form.reset();
+            setTimeout(() => {
+                result.style.display = "none";
+            }, 3000);
+        });
 });
 
 const emailIcon = document.querySelector('.email-icon');
